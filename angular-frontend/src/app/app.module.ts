fb511e493f76c13ccd43ead440fd31144f44f307
sanskriti2 <55802292+sanskriti2@users.noreply.github.com>
2021-07-22 11:48:03 +0530

    Add files via upload

---
+++ angular-frontend/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { FormsModule }   from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+
+import { TodoListComponent } from './todo-list.component';
+import { TodoService } from './todo.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoListComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [TodoService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
